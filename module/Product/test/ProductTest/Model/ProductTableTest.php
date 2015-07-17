417d52ccf17bab66e29edd61ba75d84d32be04d9
root <root@devel.solvegen.org>
2015-07-17 08:17:32 -0500

    newzend_9_commit(redirect)

--- module/Product/test/ProductTest/Model/ProductTableTest.php
+++ module/Product/test/ProductTest/Model/ProductTableTest.php
@@ -1,10 +1,8 @@
 <?php
 // module/Product/test/ProductTest/Model/ProductTableTest.php:
 namespace Product\Model;
-
 use Zend\Db\ResultSet\ResultSet;
 use PHPUnit_Framework_TestCase;
-
 class ProductableTest extends PHPUnit_Framework_TestCase
 {
     public function testFetchAllReturnsAllProducts()
@@ -16,9 +14,7 @@ class ProductableTest extends PHPUnit_Framework_TestCase
                          ->method('select')
                          ->with()
                          ->will($this->returnValue($resultSet));
-
         $productTable = new ProductTable($mockTableGateway);
-
         $this->assertSame($resultSet, $productTable->fetchAll());
     }
     public function testCanRetrieveAnProductByItsId()
@@ -29,58 +25,46 @@ class ProductableTest extends PHPUnit_Framework_TestCase
                                     'description'  => 'In My Dreams'
                                     'price' => '22',
                                     'image' => 'bbb',));
-
         $resultSet = new ResultSet();
         $resultSet->setArrayObjectPrototype(new Product());
         $resultSet->initialize(array($product));
-
         $mockTableGateway = $this->getMock('Zend\Db\TableGateway\TableGateway', array('select'), array(), '', false);
         $mockTableGateway->expects($this->once())
                          ->method('select')
                          ->with(array('id' => 123))
                          ->will($this->returnValue($resultSet));
-
         $productTable = new ProductTable($mockTableGateway);
-
         $this->assertSame($product, $productTable->getProduct(123));
     }
-
     public function testCanDeleteAnProductByItsId()
     {
         $mockTableGateway = $this->getMock('Zend\Db\TableGateway\TableGateway', array('delete'), array(), '', false);
         $mockTableGateway->expects($this->once())
                          ->method('delete')
                          ->with(array('id' => 123));
-
         $productTable = new ProductTable($mockTableGateway);
         $productTable->deleteProduct(123);
     }
-
     public function testSaveProductWillInsertNewProductsIfTheyDontAlreadyHaveAnId()
     {
         $productData = array('product_name' => 'The Military Wives', 'description' => 'In My Dreams', 'price' => '22', 'image' => 'bbb');
         $product     = new Product();
         $product->exchangeArray($productData);
-
         $mockTableGateway = $this->getMock('Zend\Db\TableGateway\TableGateway', array('insert'), array(), '', false);
         $mockTableGateway->expects($this->once())
                          ->method('insert')
                          ->with($productData);
-
         $productTable = new ProductTable($mockTableGateway);
         $productTable->saveProduct($product);
     }
-
     public function testSaveProductWillUpdateExistingProductsIfTheyAlreadyHaveAnId()
     {
         $productData = array('id' => 123, 'product_name' => 'The Military Wives', 'description' => 'In My Dreams', 'price' => '22', 'image' => 'bbb');
         $product     = new Product();
         $product->exchangeArray($productData);
-
         $resultSet = new ResultSet();
         $resultSet->setArrayObjectPrototype(new Product());
         $resultSet->initialize(array($product));
-
         $mockTableGateway = $this->getMock('Zend\Db\TableGateway\TableGateway',
                                            array('select', 'update'), array(), '', false);
         $mockTableGateway->expects($this->once())
@@ -91,25 +75,20 @@ class ProductableTest extends PHPUnit_Framework_TestCase
                          ->method('update')
                          ->with(array('product_name' => 'The Military Wives', 'description' => 'In My Dreams', 'price' => '22', 'image' => 'bbb'),
                                 array('id' => 123));
-
         $productTable = new ProductTable($mockTableGateway);
         $productTable->saveProduct($product);
     }
-
     public function testExceptionIsThrownWhenGettingNonexistentProduct()
     {
         $resultSet = new ResultSet();
         $resultSet->setArrayObjectPrototype(new Product());
         $resultSet->initialize(array());
-
         $mockTableGateway = $this->getMock('Zend\Db\TableGateway\TableGateway', array('select'), array(), '', false);
         $mockTableGateway->expects($this->once())
                          ->method('select')
                          ->with(array('id' => 123))
                          ->will($this->returnValue($resultSet));
-
         $productTable = new ProductTable($mockTableGateway);
-
         try
         {
             $productTable->getProduct(123);
@@ -119,7 +98,6 @@ class ProductableTest extends PHPUnit_Framework_TestCase
             $this->assertSame('Could not find row 123', $e->getMessage());
             return;
         }
-
         $this->fail('Expected exception was not thrown');
     }
 }
